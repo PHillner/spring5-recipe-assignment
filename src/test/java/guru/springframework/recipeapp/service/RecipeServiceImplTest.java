9ca106f0c918ee25cb6871b55b1d6e01e45c6a2b
Patrik Hillner <patrik.hillner@vincit.fi>
2020-05-08 08:58:33 +0300

    Add tests for both service and controller

---
+++ src/test/java/guru/springframework/recipeapp/service/RecipeServiceImplTest.java
@@ -0,0 +1,47 @@
+package guru.springframework.recipeapp.service;
+
+import guru.springframework.recipeapp.model.Recipe;
+import guru.springframework.recipeapp.repositories.RecipeRepository;
+import guru.springframework.recipeapp.services.RecipeServiceImpl;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
+
+public class RecipeServiceImplTest {
+
+    RecipeServiceImpl recipeService;
+
+    @Mock
+    RecipeRepository recipeRepository;
+
+
+    @BeforeEach
+    public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
+
+        recipeService = new RecipeServiceImpl(recipeRepository);
+    }
+
+    @Test
+    public void getRecipes() throws Exception {
+
+        Recipe recipe = new Recipe();
+        Set<Recipe> receipesData = new HashSet<>();
+        receipesData.add(recipe);
+
+        when(recipeService.getRecipes()).thenReturn(receipesData);
+
+        Set<Recipe> recipes = recipeService.getRecipes();
+
+        assertEquals(recipes.size(), 1);
+        verify(recipeRepository, times(1)).findAll();
+    }
+
+}
