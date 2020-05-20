9285264905a5d2c6af08223c254f3cc1d0468198
Patrik Hillner <patrik.hillner@vincit.fi>
2020-05-20 15:29:57 +0300

    Implement dynamic difficulty enumeration in recipe form
    * Add difficulty string formatter

--- src/main/java/guru/springframework/recipeapp/model/Difficulty.java
+++ src/main/java/guru/springframework/recipeapp/model/Difficulty.java
@@ -2,5 +2,10 @@ package guru.springframework.recipeapp.model;
 
 public enum Difficulty {
 
-    EASY, MODERATE, HARD
+    EASY, MODERATE, HARD;
+
+    public String formattedName() {
+        String difficultyString = this.toString();
+        return difficultyString.substring(0,1).toUpperCase() + difficultyString.substring(1).toLowerCase();
+    }
 }
